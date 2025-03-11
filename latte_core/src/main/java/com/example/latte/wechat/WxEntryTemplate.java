18838f2a3ae9440eefaf46931616880c8677e74f
luakert <ninjass@sina.com>
2025-03-12 01:19:26 +0800

    occur

---
+++ latte_core/src/main/java/com/example/latte/wechat/WxEntryTemplate.java
@@ -0,0 +1,11 @@
+package com.example.latte.wechat;
+
+import com.example.latte.activity.ProxyActivity;
+import com.example.latte.delegate.LatteDelegate;
+
+public class WxEntryTemplate extends ProxyActivity {
+    @Override
+    public LatteDelegate setRootDelegate() {
+        return null;
+    }
+}
